d6bc68e29b0e959c4ed8acc5529c0941d642d221
YaDimaseks <iaremus.dk@phystech.edu>
2020-10-16 11:25:03 +0300

    try to do 2 proccess

--- microsha/main.cpp
+++ microsha/main.cpp
@@ -1,28 +1,59 @@
-#include <iostream>
 #include <stdio.h>
-//#include <unistd.h>
-//#include <errno.h>
-//#include <sys/types.h>
+#include <process.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/types.h>
 #include <vector>
- 
+#include <string>
+#include <algorithm>
+#include <iostream>
+#include <sstream>
+
+
 using namespace std;
 
-vector<string> parsing(string s, const string& delim){
-    size_t pos = 0;
-    string token;
+vector<string> parsing(string s, const string& delimeters){
     vector<string> ans;
-    while ((pos = s.find(delim)) != std::string::npos) {
-        token = s.substr(0, pos);
-        ans.push_back(token);
-        s.erase(0, pos + delim.length());
+    for (string::iterator iter = s.begin(); iter != s.end(); iter++) {
+        if (delimeters.find(*iter) != std::string::npos) {
+            *iter = '\n';
+        }
     }
-    if (!s.empty()) { ans.push_back(s); }
+    istringstream ist(s);
+    while (getline(ist, s)) {
+        istringstream tmp(s);
+        string token;
+        tmp >> token;
+        if (token != "") ans.push_back(token);
+    }
     return ans;
 }
 
 int main() {
-    vector<string> v = parsing("cat < a | cat > b | cat", " ");
-    for (size_t i = 0; i < v.size(); i++) {
-        cout << v[i] + '\n';
+    pid_t pid_main; //PID of the process
+
+
+    string in;
+    printf(">");
+    while (getline(cin, in)) {
+        int fd[2];
+        pipe(fd);
+        pid_t pid = fork();
+        if (pid != 0) {
+            close(fd[0]);
+            dup2(fd[1], 1);
+        }
+        else {
+
+        }
+
+        //парсинг строки по пробелу и табуляции
+        vector<string> v = parsing(in, " \t");
+
+        for (auto a : v) {
+            cout << a << "\n";
+        }
+        printf(">");
     }
+
 }
